33353020bd94607f3e741c58858474d0ce94691c
GalacticTube <51178800+GalacticTube@users.noreply.github.com>
2020-01-03 00:31:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
@@ -24,7 +23,7 @@ client.on("message", async message => {
   
   if(message.author.bot) return;
   
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if(message.content.indexOf(-) !== 0) return;
   
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
