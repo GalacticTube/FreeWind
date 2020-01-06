d0f82fe433c89aecda79a7fe39417525383a5fac
GalacticTube <51178800+GalacticTube@users.noreply.github.com>
2020-01-06 11:55:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,29 @@ client.on('message', message => {
 
 });
 
+
+client.on('message', message => {
+
+    if (message.content === 'XD') {
+
+       message.reply('z ciebie');
+
+       }
+
+});
+
+
+client.on('message', message => {
+    // If the message is '!rip'
+    if (message.content === 'parówa') {
+        // Create the attachment using Attachment
+        const attachment = new Attachment('https://i2.wp.com/www.jegoeminencja.pl/wp-content/uploads/2013/07/pluszowy-fiut.jpeg');
+        // Send the attachment in the message channel
+        message.channel.send(attachment);
+    }
+});
  
+
 
 // THIS  MUST  BE  THIS  WAY
 
